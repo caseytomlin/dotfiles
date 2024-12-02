bc5e10d96e29f38cf3344dd96b0eb781c0bb756d
caseytomlin <casey.tomlin@bayer.com>
2024-12-02 11:43:00 +0000

    Update .chezmoiscripts/zsh-plugins.sh

--- .chezmoiscripts/run_once_install-fzf.sh
+++ .chezmoiscripts/run_once_install-fzf.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-if  command -v fzf >/dev/null 2>&1; then 
-    echo "starship already installed"
-else
-    sudo apt update && sudo apt install fzf
-fi
+sudo apt update
+sudo apt install fzf
